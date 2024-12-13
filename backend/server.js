f160986d23aa3ac17ea5a4711780f0e4a93e8890
JustJJaye <jijustin@hotmail.com>
2024-12-13 15:58:31 -0800

    Setup Initial Unit Testing

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,10 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const app = require("./app");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
-const recipeRoutes = require("./routes/recipeRoutes");
 
-dotenv.config();
 connectDB();
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-
-app.use("/api/users", userRoutes);
-app.use("/api/recipes", recipeRoutes);
-
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === "production" ? null : err.stack,
-    });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-app.listen(3000);
